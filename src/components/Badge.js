531666b710af8976cc109f55647d4e0d32e5016c
eloigil <eloigil6@gmail.com>
2020-01-01 20:35:07 +0100

    badge component working

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,8 +1,27 @@
 import React from "react";
 
+// import confLogo from "../";
 class Badge extends React.Component {
   render() {
-    return <h1>Badge</h1>;
+    return (
+      <div>
+        <div>{/* <img src={confLogo} alt="Logo de la conferencia" /> */}</div>
+
+        <div>
+          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+          <h1>
+            Richard <br /> Kaufman
+          </h1>
+        </div>
+
+        <div>
+          <p>Frontend Engineer</p>
+          <p>@sparragus</p>
+        </div>
+
+        <div>#platziconf</div>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -8,17 +8,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Eloi";
+import Badge from "./components/Badge";
+
+// const name = "Eloi";
 // const element = React.createElement("h1", {}, `Hola, me llamo ${name}`);
 // const jsx = <h1>Hola, me llamo {2 + 2}</h1>;
-const jsx = (
-  <div>
-    <h1>Hola, me llamo {name}</h1>
-    <p>Soy ingeniero front end</p>
-  </div>
-);
+// const jsx = (
+//   <div>
+//     <h1>Hola, me llamo {name}</h1>
+//     <p>Soy ingeniero front end</p>
+//   </div>
+// );
 
 const container = document.getElementById("app");
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(jsx, container);
+ReactDOM.render(<Badge />, container);
