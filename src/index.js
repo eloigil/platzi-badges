3d0a65da9942c35c682852ce3150882bbea50e53
eloigil <eloigil6@gmail.com>
2020-01-01 21:06:27 +0100

    props

--- src/index.js
+++ src/index.js
@@ -23,4 +23,12 @@ import Badge from "./components/Badge";
 const container = document.getElementById("app");
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(<Badge />, container);
+ReactDOM.render(
+  <Badge
+    firstName="Eloi"
+    lastName="Gil"
+    jobTitle="Frontend Hero"
+    twitter="lalalal"
+  />,
+  container
+);
