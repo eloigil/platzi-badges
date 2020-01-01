b2fb2e53d05e816a70a99e81199b36dc06dfc361
eloigil <eloigil6@gmail.com>
2020-01-01 20:23:15 +0100

    jsx

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@
 
 // container.appendChild(element);
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+const name = "Eloi";
+// const element = React.createElement("h1", {}, `Hola, me llamo ${name}`);
+// const jsx = <h1>Hola, me llamo {2 + 2}</h1>;
+const jsx = (
+  <div>
+    <h1>Hola, me llamo {name}</h1>
+    <p>Soy ingeniero front end</p>
+  </div>
+);
 
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
