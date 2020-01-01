f0add5adb15e41e976cdb854f456248a20504929
eloigil <eloigil6@gmail.com>
2020-01-01 20:37:33 +0100

    image imported

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-// import confLogo from "../";
+import confLogo from "../images/badge-header.svg";
 class Badge extends React.Component {
   render() {
     return (
       <div>
-        <div>{/* <img src={confLogo} alt="Logo de la conferencia" /> */}</div>
+        <div>
+          <img src={confLogo} alt="Logo de la conferencia" />
+        </div>
 
         <div>
           <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
