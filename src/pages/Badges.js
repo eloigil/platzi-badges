473ec35df9d7a2b0e5aad026b3325b36855651c7
eloigil <eloigil6@gmail.com>
2020-01-02 23:57:13 +0100

    badges page created

---
+++ src/pages/Badges.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+class Badges extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="Badges">
+          <div className="Badges__hero">
+            <div className="Badges__container">
+              <img src={} alt="Conf logo" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Badges;
